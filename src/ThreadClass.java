e880d3669689fa5a7f7ff07b7d403187bc41861e
Pankaj <pankajisadeveloper@gmail.com>
2016-11-02 13:28:34 +0530

    Done commenting in ThreadClass.

--- src/ThreadClass.java
+++ src/ThreadClass.java
@@ -1,22 +1,38 @@
 /**
- * Created by root on 1/11/16.
+ * Created by Pankaj on 1/11/16.
  */
 public class ThreadClass extends Thread {
+    
+    // Variable to decide whehter it is a reader thread or a writer thread
     private char readOrWrite = 'r';
+    // Semaphore variables
     private static int wrt_semaphore = 1, mutex_semaphore = 1;
+    // number of readers
     private static int reader_count = 0;
+    // checks if current thread is waiting
     private int waiting = 0;
+    // stores sleep time in milliseconds
     private long writerSleepTime = 5000, readerSleepTime = 4000;
+    // part of UI
     private String line_break = "------------------x-----------------";
-
+    
+    /*
+        Function name: readOrWrite
+        Argument: char variable with value = 'r' if it is reader thread and value = 'w' if writer thread
+        Returns: void
+        Description: This function to ensure that a thread is always a reader or a writer thread.
+    */
     public void readOrWrite(char value) {
         readOrWrite = value;
     }
-
+    
     @Override
     public void run() {
         super.run();
         if (readOrWrite == 'w') {
+            // Operations to be done if thread is writer thread
+            
+            // isBusy checks if read or write permission can be granted.
             while (isBusy(wrt_semaphore)){
                 try {
                     sleep(2000);
@@ -30,6 +46,7 @@ public class ThreadClass extends Thread {
             signalWriter();
             signalReader();
         } else if (readOrWrite == 'r') {
+            // Operations to be done if thread is reader thread
             while (isBusy(mutex_semaphore)) {
                 try {
                     sleep(1000);
@@ -37,17 +54,28 @@ public class ThreadClass extends Thread {
                     e.printStackTrace();
                 }
             }
+            
+            // If reader_count is 0, that means write permissions need to be unlocked. 
+            // Further threads don't need to check this.
             if (reader_count == 0)
                 waitWriter();
             waitReader();
+            // Increase reader_count.
             reader_count++;
             signalReader();
             readData();
         }
     }
 
+    /*
+        Function name: isBusy
+        Argument: int variable which stores value of semaphore
+        Returns: True if thread needs to wait and false if thread can continue processing.
+        Description: Function to check if thread needs to wait or if it can continue processing.
+    */
     private boolean isBusy(int semaphore) {
         if (semaphore != 1 && waiting == 0) {
+            // Current thread is in waiting state.
             waiting = 1;
             System.out.println(line_break);
             System.out.println("Thread " + getId() + " waiting.");
@@ -55,7 +83,13 @@ public class ThreadClass extends Thread {
         }
         return semaphore != 1;
     }
-
+    
+    /*
+        Function name: waitWriter
+        Argument: No argument.
+        Returns: void
+        Description: Changes the value of write semaphore so as to lock read and write privileges.
+    */    
     private void waitWriter() {
         System.out.println(line_break);
         System.out.println("Write privilege Locked");
@@ -63,7 +97,15 @@ public class ThreadClass extends Thread {
         wrt_semaphore = 0;
     }
 
+    
+    /*
+        Function name: writeData
+        Argument: No argument.
+        Returns: void
+        Description: Code to access database and modify contents in it.
+    */    
     private void writeData() {
+        // Thread removed from waiting state
         waiting = 0;
         System.out.println(line_break);
         System.out.println("Thread " + getId() + " is writing.");
@@ -78,6 +120,13 @@ public class ThreadClass extends Thread {
         System.out.println(line_break);
     }
 
+    
+    /*
+        Function name: signalWriter
+        Argument: No argument.
+        Returns: void
+        Description: Changes the value of write semaphore so as to unlock read and write privileges.
+    */    
     private void signalWriter() {
         System.out.println(line_break);
         System.out.println("Write privilege Unlocked");
@@ -85,6 +134,13 @@ public class ThreadClass extends Thread {
         wrt_semaphore = 1;
     }
 
+    
+    /*
+        Function name: waitReader
+        Argument: No argument.
+        Returns: void
+        Description: Changes the value of read semaphore so as to lock read privileges.
+    */    
     private void waitReader() {
         System.out.println(line_break);
         System.out.println("Read privilege Locked");
@@ -92,6 +148,13 @@ public class ThreadClass extends Thread {
         mutex_semaphore = 0;
     }
 
+    
+    /*
+        Function name: signalReader
+        Argument: No argument.
+        Returns: void
+        Description: Changes the value of read semaphore so as to unlock read privileges.
+    */    
     private void signalReader() {
         System.out.println(line_break);
         System.out.println("Read privilege Unlocked");
@@ -99,6 +162,13 @@ public class ThreadClass extends Thread {
         mutex_semaphore = 1;
     }
 
+    
+    /*
+        Function name: readData
+        Argument: No argument.
+        Returns: void
+        Description: Access database to read its content without modifying it.
+    */    
     private void readData() {
         System.out.println(line_break);
         System.out.println("Thread " + getId() + " is reading.");
@@ -111,6 +181,7 @@ public class ThreadClass extends Thread {
         System.out.println(line_break);
         System.out.println("Thread " + getId() + " has done reading.");
         System.out.println(line_break);
+        // Condition to check if all reader threads have done processing. If yes, then unlock write privilege.
         if (--reader_count == 0)
             signalWriter();
     }
